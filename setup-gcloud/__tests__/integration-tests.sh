9e9a766568897cd0e47e2a85a58b1846912ddbfc
craigdbarber <craigbarber@google.com>
2020-01-29 11:16:22 -0800

    debugging win32 int tests

--- setup-gcloud/__tests__/integration-tests.sh
+++ setup-gcloud/__tests__/integration-tests.sh
@@ -27,9 +27,9 @@ gcloud projects list > /dev/null && echo "Passed."
 win2lin () { f="${1/C://c}"; printf '%s\n' "${f//\\//}"; }
 
 format_arch_path() {
-    if [$PROCESSOR_ARCHITECTURE == 'AMD64']; then
+    if [[$PROCESSOR_ARCHITECTURE -e 'AMD64']]; then
         printf 'x64'
-    elif [$PROCESSOR_ARCHITECTURE == 'X86']; then
+    elif [[$PROCESSOR_ARCHITECTURE -e 'X86']]; then
         printf 'x86'
     else
         echo "Unsupported architecture: $PROCESSOR_ARCHITECTURE"
@@ -40,5 +40,4 @@ format_arch_path() {
 # Ensure gsutil was properly configured
 gsutil_cmd=$(which gsutil 2> /dev/null || "/usr/bin/pwsh $(win2lin $RUNNER_TOOL_CACHE)/gcloud/$GCLOUD_SDK_VERSION/$(format_arch_path)/bin/gsutil.ps1")
 echo "Testing gsutil..."
-echo "cmd: $gsutil_cmd ls gs://cloud-sdk-release"
 eval "$gsutil_cmd ls gs://cloud-sdk-release" > /dev/null && echo "Passed."
